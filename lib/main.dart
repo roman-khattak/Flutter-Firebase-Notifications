69a4529cd29f89163f338b7c439e64758a650d3d
roman-khattak <roman.softwareengineer@gmail.com>
2023-05-24 11:06:08 +0500

    This is my Flutter code for Firebase Push Notifications.
    In this code the Notifications are displayed when app is in Foreground, background or killed.
    
    When the notification is clicked so certain action is performed, i.e., redirecting user to next screen while passing the data/payload coming from Firebase Cloud Messaging (FCM), with it as well.
    
    The user can also send notifications from one device to another using the Token of the recipient device.

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Firebase Notification Application',
+      title: 'Flutter Firebase Notification Application',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
